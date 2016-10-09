1d8ea30a0a3eebb8a3653e4c046e2cf0e1672a71
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-10-09 16:45:20 +0300

    Added 'headers' parameter for pass custom headers to websocket connection (#4)

--- main.go
+++ main.go
@@ -13,14 +13,23 @@ import (
 )
 
 var url = flag.String("url", "", "-url ws://127.0.0.1")
+var headers = flag.String("headers", "", "-headers name1=value1,name2=value2")
 
 func main() {
 	flag.Parse()
 	if *url == "" {
 		log.Fatal("URL can't be empty.")
 	}
+	headersMap := make(map[string]string)
+	if *headers != "" {
+		headersSeq := strings.Split(*headers, ",")
+		for _, headerString := range headersSeq {
+			splittedHeader := strings.Split(headerString, "=")
+			headersMap[splittedHeader[0]] = splittedHeader[1]
+		}
+	}
 	done := make(chan bool)
-	c := NewClient(*url)
+	c := NewClient(*url, headersMap)
 
 	go func() {
 		for {
@@ -52,9 +61,12 @@ func main() {
 
 }
 
-func NewClient(url string) *websocket.Conn {
+func NewClient(url string, headers map[string]string) *websocket.Conn {
 	r, _ := http.NewRequest("GET", url, nil)
 	r.Header.Add("Content-Type", "application/json")
+	for key, value := range headers {
+		r.Header.Add(key, value)
+	}
 	c, _, err := websocket.DefaultDialer.Dial(url, r.Header)
 	if err != nil {
 		log.Fatal("errrr ", err)
